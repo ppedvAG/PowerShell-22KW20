d090f4e96b174e53d6be4c01bd63734bef663bcd
Stefan Ober <StefanO@ppedv.de>
2022-05-18 13:55:15 +0200

    Skript mit Verbose Ausgabe

--- Teil2/02_SkriptBeispiel/get-eventlog.ps1
+++ Teil2/02_SkriptBeispiel/get-eventlog.ps1
@@ -8,6 +8,9 @@ param(
 [string]$ComputerName = "localhost"
 )
 
+#Write-Verbose ermöglicht mir zusätzliche Informationen auszugeben wenn das Skript mit -verbose aufgerufen wird
+Write-Verbose -Message "Vom User wurde folgendes angegeben: $EventId | $Newest | $ComputerName" 
+
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
 
