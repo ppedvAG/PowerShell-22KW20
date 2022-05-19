42b31d7b486aede5b3b3d9a98d8ff9a256347882
Stefan Ober <StefanO@ppedv.de>
2022-05-19 13:32:16 +0200

    Fehlerbehandlung

--- Teil2/03_SkriptUebung/MoeglicheLoesung.ps1
+++ Teil2/03_SkriptUebung/MoeglicheLoesung.ps1
@@ -37,8 +37,6 @@ if(Test-Path -Path $TestDirPath -PathType Container)
     {
         #terminierender Fehler
         throw "Ordner bereits vorhanden. Ziehen Sie ihn bedacht das Skript mit -Force auszuführen"
-        #nicht terminierender Fehler
-        Write-Error -ErrorAction Stop 
     }
 }
 
